7f1b84143b57d4ec977455c5328d56e6566174b6
nebly2 <127355682+nebly2@users.noreply.github.com>
2023-09-20 02:19:48 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,42 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  generateCard();
+};
+
+const generateCard = () => {
+  const numberTag = document.getElementsByClassName("number")[0];
+  const topSuitTag = document.querySelector(".top-suit");
+  const bottomSuitTag = document.querySelector(".bottom-suit");
+  const suits = ["&hearts;", "&diams;", "&clubs;", "&spades;"];
+  const values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+
+  numberTag.innerHTML = randomValue;
+  topSuitTag.innerHTML = randomSuit;
+  bottomSuitTag.innerHTML = randomSuit;
+
+  if (randomSuit == "&hearts;" || randomSuit == "&diams;") {
+    topSuitTag.style.color = "red";
+    bottomSuitTag.style.color = "red";
+  } else {
+    topSuitTag.style.color = "black";
+    bottomSuitTag.style.color = "black";
+  }
 };
